0cc2b4723510a2fc06de9aac2d1d165953749894
guasmo <juancalveti4@gmail.com>
2026-01-31 19:49:01 -0500

    chore(config): update build configuration, dependencies and git ignore rules

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.getcapacitor.myapp',
   appName: 'front_GreenBox0.2',
   webDir: 'www',
   server: {
